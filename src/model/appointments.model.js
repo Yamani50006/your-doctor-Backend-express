fadd460661798ce4e53853d9c3ce228b69072413
Yamani50006 <Yamani5006@gmail.com>
2025-12-10 12:54:01 +0300

    feat: add appointments model with CRUD operations.

--- src/model/appointments.model.js
+++ src/model/appointments.model.js
@@ -3,12 +3,12 @@ import pool from "../config/db.js";
 export const AppointmentsModel={
 
     async findAll(){
-        const result=await pool.query("SELECT * FROM appointments");
+        const result=await pool.query('SELECT  a.id AS appointment_id,  a.patient_id,a.doctor_id, a.scheduled_at::date AS date,a.scheduled_at AS time,a.status,a.type,a.created_at AS appointment_created_at,dp.id AS doctor_profile_id, dp.full_name AS doctor_name,u.email AS doctor_email,u.phone AS doctor_phone,u.role AS doctor_role,s.name AS specialization,dp.qualifications,dp.years_experience,dp.rating,dp.reviews_count,dp.consultation_fee,dp.bio,dp.languages,dp.is_verified,dp.created_at AS doctor_created_at FROM appointments a JOIN doctor_profiles dp ON dp.id = a.doctor_id JOIN users u ON u.id = dp.user_id LEFT JOIN specialties s ON s.id = dp.specialty_id ORDER BY a.scheduled_at ASC');
         return result.rows;
     },
     
     async findById(id){
-        const result=await pool.query("SELECT * FROM appointments WHERE id=$1",[id]);
+        const result=await pool.query("SELECT  a.id AS appointment_id,  a.patient_id,a.doctor_id, a.scheduled_at::date AS date,a.scheduled_at AS time,a.status,a.type,a.created_at AS appointment_created_at,dp.id AS doctor_profile_id, dp.full_name AS doctor_name,u.email AS doctor_email,u.phone AS doctor_phone,u.role AS doctor_role,s.name AS specialization,dp.qualifications,dp.years_experience,dp.rating,dp.reviews_count,dp.consultation_fee,dp.bio,dp.languages,dp.is_verified,dp.created_at AS doctor_created_at FROM appointments a JOIN doctor_profiles dp ON dp.id = a.doctor_id JOIN users u ON u.id = dp.user_id LEFT JOIN specialties s ON s.id = dp.specialty_id WHERE a.id = $1",[id]);
         return result.rows[0];
     },
     
